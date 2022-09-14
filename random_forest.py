84917948a7acfb0e925d3754ab111d6853c3a973
David Newman <dnewmanau@gmail.com>
2022-09-14 17:13:32 +1000

    Added number of features variable

--- random_forest.py
+++ random_forest.py
@@ -1,5 +1,6 @@
-from random import seed, randrange
 import pandas as pd
+from random import seed, randrange
+from math import sqrt, log2
 
 
 def load_data(csv):
@@ -9,28 +10,28 @@ def load_data(csv):
     # get the response column
     response = df.iloc[:, -1].tolist()
 
-    # extract only the unique response variables
+    # extract the unique response variables
     unique_resp = set(response)
 
-    # encode response variables
-    encoded_resp = dict()
+    # encode the response variables
+    response_dict = dict()
     for encode, decode in enumerate(unique_resp):
-        print(decode)
-        encoded_resp[decode] = encode
+        response_dict[decode] = encode
 
-    # convert data frame rows into lists
+    # convert dataframe rows into a list of lists
     data = list()
     for row in range(len(df)):
-        df.iloc[row, -1] = encoded_resp[df.iloc[row, -1]]
+        df.iloc[row, -1] = response_dict[df.iloc[row, -1]]  # encode response
         data.append(df.iloc[row].tolist())
 
-    return [encoded_resp, data]
+    return [response_dict, data]
 
 
 def main():
-    num_features = 0  # either sqrt or log base 2
-
-    encoded_resp, data = load_data("sonar.csv")
+    response_dict, data = load_data("data.csv")
+    # either square root or log base 2
+    sqrt_features = sqrt(len(data[0]) - 1)
+    log2_features = log2(len(data[0]) - 1)
 
 
 if __name__ == "__main__":
